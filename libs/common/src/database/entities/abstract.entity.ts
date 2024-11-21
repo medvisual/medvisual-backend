6a9e42d9cbd19bc3d127dd0cef75a7f4d070c8f8
nikameru <nika.ra.txt@gmail.com>
2024-11-22 00:51:53 +0300

    Changed column name in AbstractEntity to maintain casing

--- libs/common/src/database/entities/abstract.entity.ts
+++ libs/common/src/database/entities/abstract.entity.ts
@@ -2,7 +2,9 @@ import { Exclude } from "class-transformer";
 import { CreateDateColumn } from "typeorm";
 
 export abstract class AbstractEntity {
-    @CreateDateColumn()
+    @CreateDateColumn({
+        name: "created_at"
+    })
     @Exclude()
     public createdAt: Date;
 }
